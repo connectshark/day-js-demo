23a920da443a42762cdf3b99d6f76ca83b605172
connectshark <bobhus394@gmail.com>
2021-09-01 18:01:15 +0800

    init

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,12 +1,14 @@
 <template>
   <h2 class="text-2xl">第一頁由此開始</h2>
+  <p>{{time}}</p>
 </template>
 
 <script>
+import Day from 'dayjs'
 export default {
   setup () {
     return {
-
+      time: Day().format()
     }
   }
 }
